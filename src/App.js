cbbe60673eecc3483485af78467e64725ebbca3c
madiha123-design <70858557+madiha123-design@users.noreply.github.com>
2021-12-09 00:37:43 +0530

    adder files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css'
 import Home from './pages/Home/Home.jsx'
+import Banner from './components/Banner/Banner'
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <Banner />
     </div>
   )
 }
---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Image from './asset/Image.svg'
+
+const Banner = () => {
+    return (
+        <section className="grid grid-cols-1 md:grid-cols-2 grid-flow-col py-24 bg-gray-light place-items-center">
+            <div className="col-span-1">
+                <div>
+                    <p className="font-semibold text-5xl mx-auto py-6">
+                        An excellence<br />centre for education</p>
+                    <p className="font-extralight text-gray-500 text-2xl mx-auto">
+                        Curiomind is the online learning platform that offers <br />
+                        various courses for your development</p>
+                </div>
+            </div>
+            <div className="col-span-1 hidden md:inline">
+                <img src={Image} alt="HeroImage"></img>
+            </div>
+        </section>
+    )
+}
+
+export default Banner
