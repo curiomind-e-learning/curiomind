00fefe2127dfc426e188fdb5ca971775c8968ba5
madiha123-design <70858557+madiha123-design@users.noreply.github.com>
2021-12-11 00:11:04 +0530

    Added courses section

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
     extend: {
       colors: {
         secondary: '#7F7F7F',
+        whitecolor: '#F7F7F7',
+        greycolor: '#C4C4C4',
+
       },
       fontFamily: {
         sans: ['sans-serif', 'Pacifico'],
