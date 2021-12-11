1884ab59b985bc1489c3976b280d847f0d6e403b
Srijan Kr. Gupta <gupta.srijan94@gmail.com>
2021-12-11 22:21:36 +0530

    fix: cards color and refactor

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -4,28 +4,24 @@ import courses from './Courses.json'
 const Courses = () => {
   return (
     <div>
-        <div className="flex justify-center pt-20 pb-10">
-            <hr style={{
-                width: "335px",
-                border: "2px solid rgba(127, 127, 127, 0.8)"}}></hr>
-        </div>
-        <div className="flex justify-center text-3xl px-8">
-            Explore Courses
-        </div>
-        {/* <div className="flex justify-center font-extralight text-center text-gray-500 text-xl px-8 py-5">
+      <div className="flex justify-center pt-20 pb-10">
+        <hr className="w-1/4 border-b-2 border-gray-400" />
+      </div>
+      <div className="flex justify-center text-3xl px-8">Explore Courses</div>
+      {/* <div className="flex justify-center font-extralight text-center text-gray-500 text-xl px-8 py-5">
             We offer a wide range of opportunity for you. See <br/> what you’ll get and your benefits
         </div> */}
-        <div className="grid sm:grid-cols-1 md:grid-cols-3 gap-10 grid-flow-row px-8 py-10 place-items-center">
-          {courses.map(({imgUrl,courseName,courseDetail})=>(
-            <div style={{ width: "270px" }} key={courseName}>
+      <div className="grid sm:grid-cols-1 md:grid-cols-3 gap-10 grid-flow-row px-8 py-10 place-items-center">
+        {courses.map(({ imgUrl, courseName, courseDetail }) => (
+          <div style={{ width: '270px' }} key={courseName}>
             <CoursesCard
               imgUrl={imgUrl}
               courseName={courseName}
               courseDetail={courseDetail}
             />
           </div>
-          ))}
-        </div>
+        ))}
+      </div>
     </div>
   )
 }
