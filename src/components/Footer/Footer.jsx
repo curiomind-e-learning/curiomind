6c722e72b18d5c26ad36c263f26bbc2d5b8d20ad
Srijan Kr. Gupta <gupta.srijan94@gmail.com>
2021-12-23 22:09:04 +0530

    update: footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,32 +1,46 @@
-import styles from './asset/Footer.module.css'
-import Socials from '../Socials/Socials.jsx'
+import Social from '../Social/Social.jsx'
+import { Link } from 'react-router-dom'
+
 const Footer = () => {
+  const menuItems = ['Services', 'About', 'Courses', 'Contact us']
+  const socialUrls = {
+    instaUrl: 'https://github.com/curiomind-e-learning',
+    fbUrl: 'https://github.com/curiomind-e-learning',
+    twitterUrl: 'https://github.com/curiomind-e-learning',
+  }
   return (
-      <footer className="grid-flow-row pt-6" id={styles.footerStyle}>
-        <div className='flex'>
-            <img src='https://www.linkpicture.com/q/address.svg' alt='location'></img>
-            <div className='text-xl pt-4 px-4'>Mountain View, California, United States</div>
-        </div>
-        <div className='flex'>
-            <img src='https://www.linkpicture.com/q/ci_phone.svg' alt='phone'></img>
-            <div className='text-xl pt-4 px-4'>236734792700208</div>
-        </div>
-        <div className={styles.footerStyle}>
-            <div className={styles.footerBottom}>
-            <div className={styles.text}>Privacy Policy | Terms of use | Honor Code</div>
-                <div className="grid place-items-end">
-                        <div className="flex justify-self-end self-end">
-                            <div className='text-2xl' style={{"marginTop":10}}>Follow us on:</div>
-                            <Socials
-                                fbUrl="https://twitter.com/dscciem"
-                                instaUrl="https://www.linkedin.com/company/gdsc-ciem"
-                                twitterUrl="https://www.instagram.com/dscciem/"
-                            />
-                        </div>
-                    </div>
-                </div>
+    <div className="w-full bottom-0 flex items-center justify-center bg-gray-200">
+      <div className="md:w-2/3 w-full px-4 text-white flex flex-col">
+        <div className="flex flex-col">
+          <div className="flex mt-24 mb-12 flex-row justify-between items-center">
+            <Link
+              to="/"
+              className="text-4xl text-blue-900"
+              style={{ fontFamily: 'Pacifico' }}
+            >
+              {' '}
+              Curiomind
+            </Link>
+            {menuItems.map((item) => (
+              <Link
+                to={item}
+                key={item}
+                className="hidden md:block cursor-pointer text-blue-900 hover:opacity-80"
+              >
+                {item}
+              </Link>
+            ))}
+            <div className="flex flex-row space-x-8 items-center justify-between">
+              <Social {...socialUrls} />
             </div>
-      </footer>
+          </div>
+          <hr className="border-blue-900 text-blue-900" />
+          <p className="w-full text-center my-12 text-blue-900">
+            Copyright © 2021 Curiomind. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
