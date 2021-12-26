cd5d34d0dfc93e488c55021781bc40ed4ed933dd
madiha123-design <70858557+madiha123-design@users.noreply.github.com>
2021-12-26 21:08:14 +0530

    Added Course page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home/Home'
 import NotFound from './pages/NotFound/NotFound'
 import SignUp from './pages/SignUp/SignUp'
 import SignIn from './pages/SignIn/SignIn'
+import Course from './pages/Course/Course'
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/signin" element={<SignIn />} />
+          <Route path="/course" element={< Course />} />
         </Routes>
         {/* <Footer /> */}
       </Router>
