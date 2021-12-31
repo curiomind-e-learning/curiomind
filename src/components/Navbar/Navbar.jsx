30e7c113b1e7af3cb47c2357a2d3763e0f0f0e30
Srijan Kr. Gupta <gupta.srijan94@gmail.com>
2021-12-31 13:39:14 +0530

    update: Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,25 @@
 import { Link } from 'react-router-dom'
+import { HiUserCircle } from 'react-icons/hi'
 
 const Navbar = () => {
   const menuItems = ['Home', 'Services', 'Courses', 'Testimonial', 'Contact us']
+  const token = sessionStorage.getItem('token')
 
   const getPath = (page) => {
     return `/#${page}`
   }
+
   return (
     <div className={`w-full fixed backdrop-blur-sm`} style={{ zIndex: '1' }}>
       <div className="flex justify-between items-center text-secondary overflow-hidden">
-        <div
-          className="leading-loose text-3xl px-8"
-          style={{ fontFamily: 'Pacifico' }}
-        >
-          Curiomind
-        </div>
+        <Link to="/">
+          <div
+            className="leading-loose text-3xl px-8"
+            style={{ fontFamily: 'Pacifico' }}
+          >
+            Curiomind
+          </div>
+        </Link>
         <div className="flex space-x-14">
           {menuItems.map((item) => (
             <div key={item} className="text-xl font-medium">
@@ -23,11 +28,19 @@ const Navbar = () => {
           ))}
         </div>
         <div className="flex space-x-3 p-3">
-          <Link to="signin">
-            <div className="rounded-full py-3 px-6 bg-purple-400 text-white shadow-sm hover:shadow-md">
-              Login
-            </div>
-          </Link>
+          {token ? (
+            <Link to="/profile">
+              <div className="">
+                <HiUserCircle size={40} className="text-blue-400" />
+              </div>
+            </Link>
+          ) : (
+            <Link to="/signin">
+              <div className="rounded-full py-3 px-6 bg-purple-400 text-white shadow-sm hover:shadow-md">
+                Login
+              </div>
+            </Link>
+          )}
         </div>
       </div>
     </div>
--- src/pages/Course/Course.jsx
+++ src/pages/Course/Course.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import Navbar from '../../components/Navbar/Navbar.jsx'
-import Footer from '../../components/Footer/Footer.jsx'
+// import Navbar from '../../components/Navbar/Navbar.jsx'
+// import Footer from '../../components/Footer/Footer.jsx'
 import CourseOverview from '../../components/CourseOverview/CourseOverview.jsx'
 
 const Course = () => {
   return (
     <>
-    {/* <Navbar/>
+      {/* <Navbar/>
     <Footer/> */}
-    <CourseOverview/>
+      <CourseOverview />
     </>
   )
 }
