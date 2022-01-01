3fd26c6d4e30408defb456a8fb82526aba793963
madiha123-design <70858557+madiha123-design@users.noreply.github.com>
2022-01-01 20:54:54 +0530

    dashboard fixed

--- src/components/CourseCard/CourseCard.jsx
+++ src/components/CourseCard/CourseCard.jsx
@@ -3,13 +3,13 @@ import Card from '../Card/Card'
 
 const CourseCard = () => {
   return (
-    <div className="grid sm:grid-cols-1 md:grid-cols-3 gap-4 grid-flow-row px-8 py-10 place-items-center">
-        {CardContent.map(({ imgUrl, text, color }) => (
-          <div style={{ width: '320px' }} key={text}>
-            <Card imgUrl={imgUrl} width={120} text={text} color={color} />
-          </div>
-        ))}
-      </div>
+    <div>
+      {CardContent.map(({ imgUrl, text, color }) => (
+        <div style={{ width: '200px' }} key={text}>
+          <Card imgUrl={imgUrl} text={text} color={color} />
+        </div>
+      ))}
+    </div>
   )
 }
 
--- src/components/WelcomeBanner/WelcomeBanner.jsx
+++ src/components/WelcomeBanner/WelcomeBanner.jsx
@@ -2,19 +2,25 @@
 import CourseCard from '../../components/CourseCard/CourseCard.jsx'
 const WelcomeBanner = () => {
   return (
-    <section className="grid grid-cols-1 md:grid-cols-2 grid-flow-col h-96 py-24 bg-gray-light place-items-center px-2" style={{
-        backgroundColor: "rgba(132,226,179,100)"}}>
-            <div className="col-span-1">
-            <div className="col-span-1 hidden md:inline">
-                <img src="https://www.linkpicture.com/q/waving-hand.svg" width={120} heght={120} alt="HeroImage"></img>
-                <p className="font-sans-serif-condensed text-5xl mx-auto py-6">
-                     Welcome Back!</p>
-            </div>
-            </div>
-            <div>
-                    <CourseCard/>
-                </div>
-        </section>
+    <div
+      className="flex flex-row justify-around py-20"
+      style={{
+        backgroundColor: 'rgba(132,226,179,100)',
+      }}
+    >
+      <div>
+        <img
+          src="https://www.linkpicture.com/q/waving-hand.svg"
+          width={120}
+          height={120}
+          alt="HeroImage"
+        ></img>
+        <p className="font-sans-serif-condensed text-5xl py-6">Welcome Back!</p>
+      </div>
+      <div>
+        <CourseCard />
+      </div>
+    </div>
   )
 }
 
