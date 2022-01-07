b3be381655f8d54fcc63838b7ce942f26ec0cd52
Srijan Kr. Gupta <gupta.srijan94@gmail.com>
2022-01-07 21:00:26 +0530

    fix: explore course cards

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -1,30 +1,51 @@
 import CoursesCard from '../CoursesCard/CoursesCard'
 import Title from '../Title/Title'
-import courses from './Courses.json'
+import Loader from '../Loader/Loader'
+import { useState, useEffect, useCallback } from 'react'
 
 const Courses = () => {
+  const [courses, setCourses] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  const fetchCourses = useCallback(async () => {
+    setLoading(true)
+    const res = await fetch(`${process.env.REACT_APP_API}/course/explore`)
+    const data = await res.json()
+    console.log(data)
+    setCourses(data)
+    setLoading(false)
+  }, [])
+
+  useEffect(() => {
+    fetchCourses()
+    return () => {
+      setCourses([])
+    }
+  }, [fetchCourses])
+
   return (
-    <div id="Courses">
-      <Title
-        title="Explore Courses"
-        desc1="Build career-relevant skills"
-        desc2="with courses from top industry experts"
-      />
-      {/* <div className="flex justify-center font-extralight text-center text-gray-500 text-xl px-8 py-5">
-            We offer a wide range of opportunity for you. See <br/> what you’ll get and your benefits
-        </div> */}
-      <div className="grid sm:grid-cols-1 md:grid-cols-3 gap-10 grid-flow-row px-8 py-10 place-items-center">
-        {courses.map(({ imgUrl, courseName, courseDetail }, index) => (
-          <div style={{ width: '270px' }} key={`${courseName}${index}`}>
-            <CoursesCard
-              imgUrl={imgUrl}
-              courseName={courseName}
-              courseDetail={courseDetail}
-            />
-          </div>
-        ))}
+    <>
+      <Loader active={loading} />
+      <div id="Courses">
+        <Title
+          title="Explore Courses"
+          desc1="Build career-relevant skills"
+          desc2="with courses from top industry experts"
+        />
+
+        <div className="grid sm:grid-cols-1 md:grid-cols-3 gap-10 grid-flow-row px-8 py-10 place-items-center">
+          {courses.map(({ imgUrl, name, category }, index) => (
+            <div style={{ width: '270px' }} key={`${name}${index}`}>
+              <CoursesCard
+                imgUrl={imgUrl}
+                courseName={name}
+                courseDetail={category}
+              />
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
