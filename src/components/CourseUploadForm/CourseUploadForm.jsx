f517594bd36e99cacce884534e787e79f13d45fc
Srijan Kr. Gupta <gupta.srijan94@gmail.com>
2022-01-08 15:33:25 +0530

    update: course upload

--- src/components/CourseUploadForm/CourseUploadForm.jsx
+++ src/components/CourseUploadForm/CourseUploadForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import storage from '../../firebase/firebase.config.js'
+import Loader from '../../components/Loader/Loader'
 
 import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage'
 
@@ -8,12 +9,13 @@ const CourseUploadForm = () => {
   const [courseName, setCourseName] = useState('')
   const [courseCategory, setCourseCategory] = useState('')
   const [courseDescription, setCourseDescription] = useState('')
+  const [loading, setLoading] = useState(false)
 
   const uploadFileAndSubmit = async () => {
     if (image == null) {
       return
     }
-
+    setLoading(true)
     const storageRef = ref(storage, `files/${image.name}`)
     const uploadTask = uploadBytesResumable(storageRef, image)
 
@@ -52,6 +54,9 @@ const CourseUploadForm = () => {
               description: courseDescription,
               imgUrl: downloadURL,
             }),
+          }).then(() => {
+            setLoading(false)
+            alert('Course Uploaded Successfully')
           })
         })
       }
@@ -64,75 +69,78 @@ const CourseUploadForm = () => {
   }
 
   return (
-    <div className="w-2/5 m-auto py-28">
-      <p className="flex justify-center text-2xl p-2 border-b-4 border-blue-500 mb-5 font-nunito">
-        UPLOAD COURSE
-      </p>
-      <form className="bg-white shadow-xl rounded px-8 pt-6 pb-8 mb-4 border-2 border-gray-200">
-        <div className="mb-4">
-          <label className="block text-gray-800 text-sm font-light mb-2">
-            Name of Course <span className="text-red-500 text-xl">*</span>
-          </label>
-          <input
-            className="shadow appearance-none border text-xs rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
-            type="text"
-            required
-            placeholder="Enter Course Name"
-            value={courseName}
-            onChange={(e) => setCourseName(e.target.value)}
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-800 text-sm font-light mb-2">
-            Course Category <span className="text-red-500 text-xl">*</span>
-          </label>
-          <input
-            className="shadow appearance-none border text-xs rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
-            type="text"
-            required
-            placeholder="Enter Course Category"
-            value={courseCategory}
-            onChange={(e) => setCourseCategory(e.target.value)}
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-800  text-sm font-light mb-2">
-            Course Descriptiom
-          </label>
-          <input
-            className="shadow appearance-none border text-xs rounded w-full py-2 px-3  leading-tight focus:outline-none focus:shadow-outline"
-            type="text"
-            required
-            placeholder="Enter Course Description"
-            value={courseDescription}
-            onChange={(e) => setCourseDescription(e.target.value)}
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-800 text-sm font-light mb-2">
-            Image Url
-          </label>
-          <input
-            className="shadow appearance-none text-xs border rounded w-full py-2 px-3  leading-tight focus:outline-none focus:shadow-outline"
-            type="file"
-            required
-            placeholder="Enter Image Url"
-            accept="image/*"
-            onChange={(e) => setImage(e.target.files[0])}
-          />
-        </div>
+    <>
+      <Loader active={loading} />
+      <div className="w-2/5 m-auto py-28">
+        <p className="flex justify-center text-2xl p-2 border-b-4 border-blue-500 mb-5 font-nunito">
+          UPLOAD COURSE
+        </p>
+        <form className="bg-white shadow-xl rounded px-8 pt-6 pb-8 mb-4 border-2 border-gray-200">
+          <div className="mb-4">
+            <label className="block text-gray-800 text-sm font-light mb-2">
+              Name of Course <span className="text-red-500 text-xl">*</span>
+            </label>
+            <input
+              className="shadow appearance-none border text-xs rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+              type="text"
+              required
+              placeholder="Enter Course Name"
+              value={courseName}
+              onChange={(e) => setCourseName(e.target.value)}
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-800 text-sm font-light mb-2">
+              Course Category <span className="text-red-500 text-xl">*</span>
+            </label>
+            <input
+              className="shadow appearance-none border text-xs rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+              type="text"
+              required
+              placeholder="Enter Course Category"
+              value={courseCategory}
+              onChange={(e) => setCourseCategory(e.target.value)}
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-800  text-sm font-light mb-2">
+              Course Descriptiom
+            </label>
+            <input
+              className="shadow appearance-none border text-xs rounded w-full py-2 px-3  leading-tight focus:outline-none focus:shadow-outline"
+              type="text"
+              required
+              placeholder="Enter Course Description"
+              value={courseDescription}
+              onChange={(e) => setCourseDescription(e.target.value)}
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-800 text-sm font-light mb-2">
+              Image Url
+            </label>
+            <input
+              className="shadow appearance-none text-xs border rounded w-full py-2 px-3  leading-tight focus:outline-none focus:shadow-outline"
+              type="file"
+              required
+              placeholder="Enter Image Url"
+              accept="image/*"
+              onChange={(e) => setImage(e.target.files[0])}
+            />
+          </div>
 
-        <div className="flex items-center justify-center">
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="submit"
-            onClick={handleSubmit}
-          >
-            Submit
-          </button>
-        </div>
-      </form>
-    </div>
+          <div className="flex items-center justify-center">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+              onClick={handleSubmit}
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
   )
 }
 
