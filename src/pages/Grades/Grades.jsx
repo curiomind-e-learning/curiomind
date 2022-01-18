f8615659b992c92cdfc331e3dc34678dad896207
madiha123-design <70858557+madiha123-design@users.noreply.github.com>
2022-01-18 14:19:57 +0530

    Grade page update

--- src/pages/Grades/Grades.jsx
+++ src/pages/Grades/Grades.jsx
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom'
 import Loader from '../../components/Loader/Loader'
 import Navbar from '../../components/Navbar/Navbar'
 import Sidebar from '../../components/Sidebar/Sidebar'
+import hurrayImg from './hurray.png'
 
 const Grades = () => {
   const [grade, setGrade] = useState({})
@@ -43,10 +44,19 @@ const Grades = () => {
         <Sidebar courseId={params.id} />
         <div className="flex flex-col col-span-4 pt-24 border-l-2 pl-5">
           <p className="text-5xl font-nunito font-extralight px-10">Grades</p>
-          {grade && (
-            <pre className="w-full py-5 px-16">
-              {JSON.stringify(grade.achievement, null, 4)}
-            </pre>
+          {grade.achievement && (
+            <div className="grid grid-cols-2">
+              <div className="font-nunito text-blackOlive p-10 pt-36">
+                <h1 className="text-3xl">Your Scores:</h1>
+                <p className="text-lg text-gray-600 pt-5">
+                  Assignment Score: {grade.achievement.score.assignment}
+                </p>
+                <p className="text-lg text-gray-600">
+                  Exam Score: {grade.achievement.score.exam}
+                </p>
+              </div>
+              <img src={hurrayImg} alt="hurray"></img>
+            </div>
           )}
         </div>
       </div>
