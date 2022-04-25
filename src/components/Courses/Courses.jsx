8b9292abe887a1d27ee3533770436d4656cb5a0d
Danish <skdanish1999@gmail.com>
2022-04-25 23:09:28 +0530

    responsiveness of courses card

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -3,6 +3,7 @@ import Title from '../Title/Title'
 import Loader from '../Loader/Loader'
 import { useState, useEffect, useCallback } from 'react'
 import { Link } from 'react-router-dom'
+import { FaAngleRight } from 'react-icons/fa'
 
 const Courses = () => {
   const [courses, setCourses] = useState([])
@@ -26,14 +27,16 @@ const Courses = () => {
   return (
     <>
       <Loader active={loading} />
-      <div id="Courses">
+      <div
+        id="Courses"
+        className="flex justify-center items-center mx-auto my-4 flex-col sm:p-10 p-5"
+      >
         <Title
           title="Explore Courses"
           desc1="Build career-relevant skills"
           desc2="with courses from top industry experts"
         />
-
-        <div className="grid sm:grid-cols-1 md:grid-cols-3 gap-10 grid-flow-row px-20 py-10 place-items-center">
+        <div className="grid gap-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 my-8">
           {courses.map(({ imgUrl, name, category, _id }, index) => (
             <Link key={`${name}${index}`} to={`/course/${_id}`}>
               <CoursesCard
@@ -44,15 +47,21 @@ const Courses = () => {
             </Link>
           ))}
         </div>
-        <div className="flex justify-end">
-          <Link
+        <Link
+          to="/allcourses"
+          className="flex flex-row items-center justify-center mt-4 border-2 rounded bg-[#E5E7EB] cursor-pointer "
+        >
+          <div
             to="/allcourses"
-            className="text-xl text-primary underline px-11 pb-3"
+            className="text-xl text-primary underline pl-5 pr-3 py-3"
             style={{ fontFamily: 'Nunito' }}
           >
             View All
-          </Link>
-        </div>
+          </div>
+          <div>
+            <FaAngleRight size="1.5rem" className="mr-2" />
+          </div>
+        </Link>
       </div>
     </>
   )
